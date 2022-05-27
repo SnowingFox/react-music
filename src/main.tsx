3596602d83fd13433c30927b3e6e893d89985303
snowingfox <1503401882@qq.com>
2022-05-27 10:54:03 +0800

    :zap: import swiper style

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import App from './App'
 import './index.css'
 import 'virtual:uno.css'
 import '@unocss/reset/tailwind.css'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 
 const container = createRoot(document.querySelector('#root')!)
 
